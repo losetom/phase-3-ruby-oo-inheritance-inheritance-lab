82fa776d65ce6980112d360851b58a9325af7e2a
Mike Loseto <losetom1998@gmail.com>
2022-10-05 13:35:03 -0400

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_reader :knowledge 
+
+    def initialize 
+        #knowledge is an instance variable, and is pointing to an empty array 
+        @knowledge = []
+    end
+
+    #method learn is defined, takes in the string 'knowledge'
+    def learn(knowledge)
+        # shovel metthod << is used here, other methods viable
+        @knowledge << knowledge
+    end
 
 end
